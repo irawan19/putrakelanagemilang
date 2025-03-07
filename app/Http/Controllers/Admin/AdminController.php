4c839dc447ee45556b5cf615e567d2065f9183f3
Irawan Agung Nugroho <irawan.nugroho@spesolution.com>
2025-03-08 01:40:09 +0700

    update admin

---
+++ app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Helpers\General;
+use Storage;
+
+class AdminController extends AdminCoreController {
+
+    public function index(Request $request) {
+        $data['hasil_kata']         = '';
+        $url_sekarang               = $request->fullUrl();
+        $data['admins']             = User::orderBy('name')->paginate(10);
+        session()->forget('hasil_kata');
+        session()->forget('halaman');
+        session(['halaman'          => $url_sekarang]);
+        return view('admin.admin.lihat',$data);
+    }
+
+    public function cari(Request $request) {
+        $hasil_kata                 = $request->cari_kata;
+        $data['hasil_kata']         = $hasil_kata;
+        $url_sekarang               = $request->fullUrl();
+        $data['admins']             = User::Where('name', 'LIKE', '%'.$hasil_kata.'%')
+                                        ->orWhere('username', 'LIKE', '%'.$hasil_kata.'%')
+                                        ->orWhere('email', 'LIKE', '%'.$hasil_kata.'%')
+                                        ->paginate(10);
+        session(['hasil_kata'		=> $hasil_kata]);
+        session(['halaman'          => $url_sekarang]);
+        return view('admin.admin.lihat', $data);
+    }
+
+    public function tambah(Request $request) {
+        return view('admin.admin.tambah');
+    }
+
+    public function prosestambah(Request $request) {
+        $aturan = [
+            'username'              => 'required|unique:users',
+            'name'                  => 'required',
+            'email'                 => 'required|unique:users',
+            'password'              => 'required|string|min:6|confirmed',
+        ];
+        $this->validate($request, $aturan);
+
+        $data = [
+            'name'               	=> $request->name,
+            'username'              => $request->username,
+            'email'              	=> $request->email,
+            'created_at'         	=> date('Y-m-d H:i:s'),
+            'password'           	=> bcrypt($request->password),
+            'remember_token'     	=> str_random(100),
+        ];
+
+        User::insert($data);
+        
+        if(request()->session()->get('halaman') != '')
+            $redirect_halaman  = request()->session()->get('halaman');
+        else
+            $redirect_halaman  = 'dashboard/admin';
+
+        return redirect($redirect_halaman);
+    }
+
+    public function edit(Request $request, $idadmin) {
+        $cek_admins = User::find($idadmin);
+        if (!empty($cek_admins)) {
+            $data['admins'] = User::find($idadmin);
+            return view('admin.admin.edit', $data);
+        } else {
+            return redirect('dashboard/slideshow');
+        }
+    }
+
+    public function prosesedit(Request $request, $idadmin) {
+        $cek_admins = User::find($idadmin);
+        if (!empty($cek_admins)) {
+            if (!empty($request->password)) {
+                $aturan = [
+                    'username'              => 'required|unique:users',
+                    'name'                  => 'required',
+                    'email'                 => 'required|unique:users',
+                    'password'              => 'required|string|min:6|confirmed',
+                ];
+                $this->validate($request, $aturan);
+        
+                $data = [
+                    'name'               	=> $request->name,
+                    'username'              => $request->username,
+                    'email'              	=> $request->email,
+                    'updated_At'         	=> date('Y-m-d H:i:s'),
+                    'password'           	=> bcrypt($request->password),
+                ];
+            } else {
+                $aturan = [
+                    'username'              => 'required|unique:users,username,'.$idadmin.',id',
+                    'name'                  => 'required',
+                    'email'                 => 'required|unique:users,email,'.$idadmin.',id',
+                ];
+                $this->validate($request, $aturan);
+        
+                $data = [
+                    'name'               	=> $request->name,
+                    'username'              => $request->username,
+                    'email'              	=> $request->email,
+                    'updated_at'         	=> date('Y-m-d H:i:s'),
+                ];
+            }
+            User::find($idadmin)->update($data);
+            
+            if(request()->session()->get('halaman') != '')
+                $redirect_halaman  = request()->session()->get('halaman');
+            else
+                $redirect_halaman  = 'dashboard/admin';
+
+            return redirect($redirect_halaman);
+        } else {
+            return redirect('dashboard/slideshow');
+        }
+    }
+
+    public function hapus($idadmin) {
+        $cek_admins = User::find($idadmin);
+        if (!empty($cek_admins)) {
+            User::find($idadmin)->delete();
+            return response()->json(['sukses' => '"sukses'], 200);
+        } else {
+            return redirect('dashboard/slideshow');
+        }
+    }
+}
---
+++ resources/views/admin/admin/tambah.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.back.app')
+@section('content')
+
+    <div class="row g-4 mb-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <strong>Tambah Admin</strong>
+                </div>
+				<form class="form-horizontal m-t-40" action="{{ URL('dashboard/admin/prosestambah') }}" method="POST">
+					{{ csrf_field() }}
+                    <div class="card-body">
+                        <div class="mb-3">
+                            <label class="form-label" for="name">Nama <b style="color:red">*</b></label>
+                            <input class="form-control {{ \App\Helpers\General::validForm($errors->first('name')) }}" id="name" type="text" name="name" value="{{Request::old('name')}}">
+                            {{\App\Helpers\General::pesanErrorForm($errors->first('name'))}}
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="username">Username <b style="color:red">*</b></label>
+                            <input class="form-control {{ \App\Helpers\General::validForm($errors->first('username')) }}" id="username" type="text" name="username" value="{{Request::old('username')}}">
+                            {{\App\Helpers\General::pesanErrorForm($errors->first('username'))}}
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="email">Email <b style="color:red">*</b></label>
+                            <input class="form-control {{ \App\Helpers\General::validForm($errors->first('email')) }}" id="email" type="email" name="email" value="{{Request::old('email')}}">
+                            {{\App\Helpers\General::pesanErrorForm($errors->first('email'))}}
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="password">Password <b style="color:red">*</b></label>
+                            <input class="form-control {{ \App\Helpers\General::validForm($errors->first('password')) }}" id="password" type="password" name="password" value="{{Request::old('password')}}">
+                            {{\App\Helpers\General::pesanErrorForm($errors->first('password'))}}
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label" for="password_confirmation">Konfirmasi Password <b style="color:red">*</b></label>
+                            <input class="form-control {{ \App\Helpers\General::validForm($errors->first('password_confirmation')) }}" id="password_confirmation" type="password" name="password_confirmation" value="{{Request::old('password_confirmation')}}">
+                            {{\App\Helpers\General::pesanErrorForm($errors->first('password_confirmation'))}}
+                        </div>
+                    </div>
+                    <div class="card-footer right-align">
+                        {{\App\Helpers\General::simpan()}}
+			          	@if(request()->session()->get('halaman') != '')
+		            		@php($ambil_kembali = request()->session()->get('halaman'))
+	                    @else
+	                    	@php($ambil_kembali = URL('dashboard/admin'))
+	                    @endif
+						{{\App\Helpers\General::batal($ambil_kembali)}}
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
